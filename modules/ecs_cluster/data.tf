555bec319daa4618062b0bca7b8ddd6c0cfbb751
Jan Costandius <jcostandius@gmail.com>
2025-01-12 17:26:39 +0200

    DEVOPS-1981 Fixed TF fetching multiple ACM certificates

--- modules/ecs_cluster/data.tf
+++ modules/ecs_cluster/data.tf
@@ -143,6 +143,8 @@ data "aws_prefix_list" "internal" {
 }
 
 data "aws_acm_certificate" "alb" {
-  count  = var.load_balancer.domain != null ? 1 : 0
-  domain = var.load_balancer.domain
+  count       = var.load_balancer.domain != null ? 1 : 0
+  domain      = var.load_balancer.domain
+  statuses    = ["ISSUED"]
+  most_recent = true
 }
