ca8582322b395b9ea313243b0c0ad66ec5dd25d5
Jan Costandius <jcostandius@gmail.com>
2025-01-12 16:41:12 +0200

    DEVOPS-1981 Changed text/text to text/plain

--- modules/ecs_cluster/main.tf
+++ modules/ecs_cluster/main.tf
@@ -300,7 +300,7 @@ resource "aws_lb_listener" "http_fwd" {
   default_action {
     type = "fixed-response"
     fixed_response {
-      content_type = "text/text"
+      content_type = "text/plain"
       message_body = "Not Found"
       status_code  = 404
     }
