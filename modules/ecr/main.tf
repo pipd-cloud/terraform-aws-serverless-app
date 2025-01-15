ee6d32f6b3a455defdb130cb8819b9e40620bbc6
Jan Costandius <jcostandius@gmail.com>
2025-01-15 10:28:59 +0200

    Fix image tag mutability variable reference in ECR repository resource

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "task" {
   name                 = "${var.id}-${var.repo_name}"
-  image_tag_mutability = var.
+  image_tag_mutability = var.image_tag_mutability
   force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
